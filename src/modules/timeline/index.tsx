b962da32accd2f57014c50255f38061bc9f7281a
Eva <eva@byallmeans.work>
2025-03-04 11:12:39 +0200

    fix: fix line animation

--- src/modules/timeline/index.tsx
+++ src/modules/timeline/index.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react';
 import { TimelineItem, TimelineItemBase } from './item';
+import { motion } from 'framer-motion';
 
 interface TimelineProps {
   data: TimelineItemBase[];
@@ -8,7 +9,13 @@ interface TimelineProps {
 export const Timeline: FC<TimelineProps> = ({ data }) => {
   return (
     <section className='relative mx-auto lg:max-w-[90%]'>
-      <div className='absolute left-1/2 -ml-[1px] hidden h-[calc(100%-60px)] w-[2px] bg-slate-500 lg:block' />
+      <motion.div
+        className='absolute left-1/2 -ml-[1px] hidden w-[2px] bg-slate-500 lg:block'
+        initial={{ height: 0 }}
+        whileInView={{ height: 'calc(100% - 60px)' }}
+        viewport={{ once: true }}
+        transition={{ duration: 2 }}
+      />
       {data.map((item, index) => (
         <TimelineItem
           key={item.year}
